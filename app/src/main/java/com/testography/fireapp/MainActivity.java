8bcaf48def6c47f49f68e1d44b142bf4422f7c52
Leo Burdianov <leo.burdianov@gmail.com>
2016-08-27 19:39:22 +0400

    Retrieve a single object

--- app/src/main/java/com/testography/fireapp/MainActivity.java
+++ app/src/main/java/com/testography/fireapp/MainActivity.java
@@ -2,13 +2,39 @@ package com.testography.fireapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mValueView;
+
+    private Firebase mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mValueView = (TextView) findViewById(R.id.valueView);
+
+        mRef = new Firebase("https://fireapp-38767.firebaseio.com/Name");
+
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                mValueView.setText(value);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
     }
 }
