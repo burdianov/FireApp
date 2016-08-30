d986cfb7d46d9b417935e0d647db9837975f0ef2
Leo Burdianov <leo.burdianov@gmail.com>
2016-08-31 00:41:53 +0400

    Retrieve multiple objects

--- app/src/main/java/com/testography/fireapp/MainActivity.java
+++ app/src/main/java/com/testography/fireapp/MainActivity.java
@@ -2,13 +2,43 @@ package com.testography.fireapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Firebase mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mRef = new Firebase("https://fireapp-38767.firebaseio.com/");
+
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map<String, String> map = dataSnapshot.getValue(Map.class);
+                String name = map.get("Name");
+                String age = map.get("Age");
+                String profession = map.get("Profession");
+
+                Log.v("E_VALUE", "Name: " + name);
+                Log.v("E_VALUE", "Age: " + age);
+                Log.v("E_VALUE", "Profession: " + profession);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
     }
 }
